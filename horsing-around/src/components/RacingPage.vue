ba89c45ce08ae0b0dc0ace45d1d34b9d84ee288b
huiminlim99 <huiminlim@u.nus.edu>
2022-10-13 19:54:07 +0800

    table headers

--- horsing-around/src/components/RacingPage.vue
+++ horsing-around/src/components/RacingPage.vue
@@ -1,7 +1,21 @@
 <!-- Just welcome to CPP -->
 
 <template>
-    <h1>Welcome to CPP</h1>
+    <h1 id = "Current">Race Table</h1>
+    <table id = "table" class = "auto-index">
+        <tr>
+            <th>Colour</th>
+            <th># - Horse Name - Desc</th>
+            <th>Bar</th>
+            <th>Last 6 Runs</th>
+            <th>Gear</th>
+            <th>Rtg (+/-)</th>
+            <th>Raceday Horse Wt (+/-)</th>
+            <th>Jockey - C Wt (Hcp Wt)</th>
+            <th>Trainer</th>
+            <th>Owner</th>
+        </tr>
+    </table><br><br>
 </template>
 
 <script>
