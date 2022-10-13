d8c2d2b5ffbff44d651411df40707e7543eb6df6
Lundypgk <Lundypgk@gmail.com>
2022-10-13 23:47:20 +0800

    Fixed the nav bar

--- horsing-around/src/App.vue
+++ horsing-around/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  
-  <div id="nav">
-    <router-link to="/">Racing</router-link> |
-    <router-link to="/horses">Horses</router-link> |
-    <router-link to="/jockey">Jockey</router-link> 
-    <div class = 'login'>
-      <router-link to="/login">Login</router-link>
+  <div class="header">
+    <div id="nav">
+      <router-link to="/">Racing</router-link> 
+      <router-link to="/horses">Horses</router-link> 
+      <router-link to="/jockey">Jockey</router-link> 
+      <div class = 'login'>
+        <router-link to="/login">Login</router-link>
+      </div>
     </div>
-  </div>
+</div>
   <router-view />
 
 </template>
@@ -38,22 +39,32 @@ export default {
 }
 
 #nav {
-  top: 0px;
   padding: 10px;
-  background-color: rgb(176, 176, 176);
-
+  background-color: #fff;
+  height: 60px;
+  display: inline-block;
+  width: 100%;
 }
 
 #nav a {
+  padding-top: 19px;
   font-weight: bold;
+  text-decoration: none;
   color: #2e3e50;
+  display: inline-block;
+  width: 8%;
+  margin: 0px 1%;
+  height: 70px;
 }
 
 #nav a.router-link-exact-active {
-  color: #750e7e
+  background-color: #F1ECEC;
+  color: #750e7e;
+  border-radius: 30px;
 }
 
 .login{
   float: right;
+  margin-right: 50px;
 }
 </style>
