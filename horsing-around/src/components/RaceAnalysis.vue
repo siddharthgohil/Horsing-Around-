eda981343bb537b5dc571d0ef883611e993f892a
siddharthgohil <48083601+siddharthgohil@users.noreply.github.com>
2022-10-13 18:15:29 +0800

    Added ability to pull data from firestore
    All news will automatically be updated from firestore instead of hard coding it

--- horsing-around/src/components/RaceAnalysis.vue
+++ horsing-around/src/components/RaceAnalysis.vue
@@ -4,44 +4,41 @@
       <!-- Add image -->
       <h2 class="HeaderTag">Race Analysis</h2>
     </div>
-    <div class="RaceAnalysisText">
-      <h4 class="pick">1st Pick: WILD BEE (10)</h4>
-      <h5 class="pickAnalysis">
-        He is a seven-time winner in his career and holds the lowest rating of
-        this field. The last time we saw him run at this distance was in July
-        where he finished fourth beaten a length just under a length to the
-        winner. Nice each way chance.
-      </h5>
-      <h4 class="pick">2nd Pick: WHAT YOU LIKE (9)</h4>
-      <h5 class="pickAnalysis">
-        All four of his victories have come on the Polytrack. He does however
-        have a few nice placings to his name in similar conditions. Winning form
-        is good form for a race like this. Nunes in the saddle a big plus for
-        him.
-      </h5>
-      <h4 class="pick">3rd Pick: GRIFFIN (3)</h4>
-      <h5 class="pickAnalysis">
-        Having his second start in this grade after a disappointing start to his
-        Singapore career. If he can bring any of his Australian form to this, he
-        can surprise and figure in the finish.
-      </h5>
-      <h4 class="pick">4th Pick: WHIZ FIZZ (7)</h4>
-      <h5 class="pickAnalysis">
-        Has been met with betting support at his last few runs. Has since
-        trialed nicely behind Jupiter Gold with the blinkers applied. Watch the
-        market again for any moves.
-      </h5>
+    <div id="RaceAnalysisText">
     </div>
   </div>
 </template>
 
 <script>
+import firebaseApp from '../firebase.js'
+import {getFirestore} from 'firebase/firestore'
+import {collection, getDocs} from 'firebase/firestore';
+
+const db = getFirestore(firebaseApp);
 export default {
   name: "RaceAnalysis",
+  created() {
+    async function display() {
+      let z = await getDocs(collection(db, "RachingAnalysisText"));
+      var container  = document.getElementById("RaceAnalysisText");
+
+      z.forEach((docs) => {
+        let yy = docs.data(); // Row data for 1
+
+        var Text = yy.Text;
+        var Title = yy.Title;
+        container.innerHTML+=('<h4 class="pick">' + Title + '</h4>');
+        container.innerHTML+=('<h5 class="pickAnalysis">' + Text + '</h5>');
+
+      });
+    }
+    display();
+
+  },
 };
 </script>
 
-<style scoped>
+<style>
 .MainHeader {
   margin: 40px;
   padding: 30px;
@@ -51,7 +48,7 @@ export default {
   text-align: left;
 }
 
-.RaceAnalysisText {
+#RaceAnalysisText {
   margin: 40px;
   padding: 30px;
   background-color: whitesmoke;
