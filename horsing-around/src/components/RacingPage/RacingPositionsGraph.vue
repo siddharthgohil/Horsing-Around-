cbf39d1d6c6cfb2d29d4bcaf5b087ea1eab15e73
Jones <joneslimwork@gmail.com>
2022-10-18 14:06:06 +0800

    updated working graph with dummy data
    no firebase data yet

--- horsing-around/src/components/RacingPage/RacingPositionsGraph.vue
+++ horsing-around/src/components/RacingPage/RacingPositionsGraph.vue
@@ -1,53 +1,96 @@
 <template>
-  <line-chart class = "history_positions" width="95%" height="500px" :data="horsedata" 
-  xtitle="Race Number" ytitle="Position" title="Recent Form of Horses" legend="right" ></line-chart>
-
+  <line-chart id = "history_positions" width="95%" height="500px" :data="recent_data" 
+  xtitle="Race Number" ytitle="Position" title="Recent Form of Horses" legend="right" loading = "Loading..."></line-chart>
 
 </template>
 
 <script>
-//import VueChartKick from "vue-chartkick";
-//new VueChartKick.LineChart("chart-1", {"2021-01-01": 11, "2021-01-02": 6})
-//  <div id="chart-1" style="height: 300px;"></div>
+//import firebaseApp from "@/firebase.js";
+//import { getFirestore } from "firebase/firestore";
+//import { collection, getDocs } from "firebase/firestore";
 
-// import { library } from 'webpack';
+//const db = getFirestore(firebaseApp);
+//let indivHorses = db.ref('previousPositions');
 
 export default {
     name:"RacingPositionsGraph",
+    // firebase: {
+    //     indivHorsesArr: indivHorses
+    // },
+
     data(){
-        return{
-            horsedata: [
+        return {
+            recent_data: [
                 {name: 'horse1', data: {"R1": 2, "R2": 1, "R3": 2, "R4": 4, "R5": 6, "R6": 1}},
                 {name: 'horse2', data: {"R1": 4, "R2": 4, "R3": 1, "R4": 5, "R5": 2, "R6": 2}},
-                {name: 'horse3', data: {"R1": 3, "R2": 5, "R3": 4, "R4": 4, "R5": "", "R6": 3}},
+                {name: 'horse3', data: {"R1": 3, "R2": 5, "R3": 4, "R4": 4, "R5": 2, "R6": 3}},
                 {name: 'horse4', data: {"R1": 4, "R2": 6, "R3": 3, "R4": 2, "R5": 1, "R6": 4}},
                 {name: 'horse5', data: {"R1": 1, "R2": 3, "R3": 2, "R4": 5, "R5": 1, "R6": 5}},
                 {name: 'horse6', data: {"R1": 5, "R2": 9, "R3": 1, "R4": 6, "R5": 2, "R6": 6}},
             ]
         }
-    },
-    // this looks very messy on a graph, need to select a few only.
-    // need to invert the graph so that 1st place finishes are higher in graph
-    // :library="{ 
-    // options: {
-    //     scales: {
-    //         yAxes: [{
-    //             ticks: {
-    //                 beginAtZero: true,
-    //                 reverse: true,
-    //                 start: 0
-    //             }
-    //         }]
+    }
+
+    //     alert('data');
+    //     return {
+    //         names: []
     //     }
-    // }}"
-}
+    // },
+    // computed: {
+    //     recent_data: function () {
+    //         let newArr = this.names
+    //         for (let item of this.indivHorses) {
+    //             newArr.push(item.name);
+    //         }
+    //         alert('computed');
+    //         return newArr
+    //     }
+    // },
+        // async function retrieveHorseData() {
+        //     let z = await getDocs(collection(db, "PreviousPositions"));
+        //     let horsedata = [];
+
+        //     z.forEach((docs) => {
+        //         let indivHorses = docs.data(); // Row data
+        //         let name_horse = indivHorses.name;
+        //         let horse_prev_1 = indivHorses.prevPos1;
+        //         let horse_prev_2 = indivHorses.prevPos2;
+        //         let horse_prev_3 = indivHorses.prevPos3;
+        //         let indivHorseData = {name: name_horse, data: {"R1": horse_prev_1, "R2": horse_prev_2, "R3": horse_prev_3}};
+        //         horsedata.push(indivHorseData);
+
+        //     });
+
+        //     return horsedata;
+        // }
+        // let horse_data_retrieved = retrieveHorseData();
+        // console.log(horse_data_retrieved);
+
+        // return { recent_data: horse_data_retrieved } 
+
+        //}
+        // this looks very messy on a graph, need to select a few only.
+        // need to invert the graph so that 1st place finishes are higher in graph
+        // :library="{ 
+        // options: {
+        //     scales: {
+        //         yAxes: [{
+        //             ticks: {
+        //                 beginAtZero: true,
+        //                 reverse: true,
+        //                 start: 0
+        //             }
+        //         }]
+        //     }
+        // }}"
+    }
 
 
 </script>
 
 <style>
 
-.history_positions{
+#history_positions{
     margin: 40px;
     padding: 30px;
     display: block;
