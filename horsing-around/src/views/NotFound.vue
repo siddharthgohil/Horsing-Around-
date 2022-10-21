2552652d551599f8d2a65815907ba7edd1e5ce13
siddharthgohil <Siddharthgohil@gmail.com>
2022-10-21 11:36:07 +0800

    Removed Old Login and updated PageNotFound

--- horsing-around/src/views/NotFound.vue
+++ horsing-around/src/views/NotFound.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <NavBar />
     <h3>Page not Found!!</h3>
-    <h4>Please click on the aobve available options</h4>
-    <LogoutComp />
+    <h4>Please click on the above available options</h4>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue";
-import LogoutComp from "@/components/LogoutComp.vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default {
   name: "NotFound",
-  components: {
-    NavBar,
-    LogoutComp,
-  },
   data() {
     return {
       user: false,
