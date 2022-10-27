60119004510de487cb6bb8ff41c6074d1fc5712a
Lundypgk <Lundypgk@gmail.com>
2022-10-27 15:14:44 +0800

    Individual Horse Page Wirwframing

--- horsing-around/src/components/HorsePage/IndividualHorseStats.vue
+++ horsing-around/src/components/HorsePage/IndividualHorseStats.vue
@@ -1,6 +1,66 @@
 <template>
   <div id="HorseStats">
-    <p>The IndividualHorseStats</p>
+    <img id="HorseMugShot" src="../../assets/images/Horse_image.png">
+      <div class="Individual_Details">
+       <h5 class="HorseName">Horse Name (yes Horse nickname or something)</h5>
+       <table id="Horse_Actual_Stats">
+        <tr>
+          <td>5yo Br AUS G SH</td>
+          <td> </td> 
+          <td>Sire </td>
+          <td>Sizzling (AUS)</td>
+        </tr>
+
+        <tr>
+          <td>MRA Brand Number</td>
+          <td>X175</td> 
+          <td>Dam </td>
+          <td>Touch Too Much (AUS)</td>
+        </tr>
+
+        <tr>
+          <td>Date Foaled</td>
+          <td>24/10/2017</td> 
+          <td>Owner</td>
+          <td>STEVEN HAROLD BURRIDGE</td>
+        </tr>
+
+        <tr>
+          <td>Current Rating</td>
+          <td>48</td> 
+          <td>Trainer </td>
+          <td>S BURRIDGE</td>
+        </tr>
+
+        <tr>
+          <td>Raing as of 1st Jan 2022</td>
+          <td> </td> 
+          <td>Current Year Stakes Money </td>
+          <td>S$9,056.25</td>
+        </tr>
+
+        <tr>
+          <td> </td>
+          <td> </td> 
+          <td> </td>
+          <td>RM0.00</td>
+        </tr>
+
+        <tr>
+          <td>Starts-1-2-3</td>
+          <td>9-0-0-1</td> 
+          <td>Total Stakes Money</td>
+          <td>S$9,056.25</td>
+        </tr>
+
+        <tr>
+          <td> </td>
+          <td> </td> 
+          <td> </td>
+          <td>RM0.00</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -11,11 +71,42 @@ export default {
 </script>
 
 <style scoped>
+#HorseMugShot {
+  width:250px;
+  margin: auto;
+  padding-left: 5%;
+  margin-left: 30px;
+}
+.HorseName {
+  margin-left: 20px;
+}
+.Individual_Details{
+  text-align: left;
+  width:80%;
+  margin-left: 15%;
+  padding: 15px;
+}
 #HorseStats {
     width: 95%;
     background: #fff;
-    min-height: 320px;
     margin: auto;
     margin-top: 20px;
+    display: grid;
+    /* grid-auto-columns: 100px; */
+    grid-template-columns: 20vh 8fr;
 }
+table {
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 15px;
+  font-weight: 4000;
+  border-collapse: collapse;
+  width: 100%;
+  margin-top: 40px;
+}
+
+td, th {
+  text-align: left;
+  padding: 0px 20px;
+}
+
 </style>
